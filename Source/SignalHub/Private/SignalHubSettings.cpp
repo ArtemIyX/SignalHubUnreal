a84bff23adbda9be535955616c0b189d9bc978f0
Wellsaik <artem.podorozhko@outlook.com>
2026-09-24 10:10:14 +0300

    feat(signalhub): label project settings section

--- Source/SignalHub/Private/SignalHubSettings.cpp
+++ Source/SignalHub/Private/SignalHubSettings.cpp
@@ -1,1 +1,32 @@
 #include "SignalHubSettings.h"
+
+#define LOCTEXT_NAMESPACE "SignalHubSettings"
+
+FName USignalHubSettings::GetContainerName() const
+{
+	return TEXT("Project");
+}
+
+FName USignalHubSettings::GetCategoryName() const
+{
+	return TEXT("Plugins");
+}
+
+FName USignalHubSettings::GetSectionName() const
+{
+	return TEXT("SignalHub");
+}
+
+#if WITH_EDITOR
+FText USignalHubSettings::GetSectionText() const
+{
+	return LOCTEXT("SectionText", "Signal Hub");
+}
+
+FText USignalHubSettings::GetSectionDescription() const
+{
+	return LOCTEXT("SectionDescription", "Configures queue and dispatch limits for per-game-instance SignalHub routing.");
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
--- Source/SignalHub/Public/SignalHubSettings.h
+++ Source/SignalHub/Public/SignalHubSettings.h
@@ -11,6 +11,15 @@ class SIGNALHUB_API USignalHubSettings final : public UDeveloperSettings
 	GENERATED_BODY()
 
 public:
+	virtual FName GetContainerName() const override;
+	virtual FName GetCategoryName() const override;
+	virtual FName GetSectionName() const override;
+
+#if WITH_EDITOR
+	virtual FText GetSectionText() const override;
+	virtual FText GetSectionDescription() const override;
+#endif
+
 	UPROPERTY(Config, EditAnywhere, Category = "Limits", meta = (ToolTip = "Runtime queue and dispatch limits applied to each new SignalHub game-instance subsystem."))
 	FSignalHubLimits Limits;
 };
